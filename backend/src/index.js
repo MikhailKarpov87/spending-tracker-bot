73743f3c6f374742cbb977340d0dbf5ab6b62512
Mikhail Karpov <mihanikk@gmail.com>
2020-02-17 10:35:42 +0300

    added centralized error handler to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const mongoURI = process.env.MONGO_URI;
+const { handleError, ErrorHandler } = require('./helpers/error');
 
 require('./models/User');
 require('./models/Operation');
@@ -17,18 +17,22 @@ mongoose
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: ['temporary-key'],
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.json());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: ['temporary-key'],
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 require('./routes/operationRoutes')(app);
 require('./routes/userRoutes')(app);
+
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
 
 if (['production'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
--- backend/src/routes/operationRoutes.js
+++ backend/src/routes/operationRoutes.js
@@ -1,37 +1,54 @@
 const mongoose = require('mongoose');
+const { ErrorHandler } = require('../helpers/error');
 // const requireLogin = require('../middlewares/requireLogin');
 
 const Operation = mongoose.model('Operation');
 
 module.exports = app => {
   app.get('/api/operations/:user_id/last_10', async (req, res) => {
-    const userId = req.params.user_id;
-    console.log(userId);
-    if (!userId) {
-      res.send(401);
+    try {
+      const userId = req.params.user_id;
+
+      if (!userId) {
+        throw new ErrorHandler(401, 'Missing user_id');
+      }
+
+      const operations = await Operation.find({ userId })
+        .sort({ createdAt: -1 })
+        .limit(10);
+
+      if (!operations) {
+        throw new ErrorHandler(500, 'Internal server error');
+      }
+
+      res.send(operations);
+    } catch (err) {
+      console.log(err);
+      throw new ErrorHandler(500, 'Internal server error');
     }
-    const operations = await Operation.find({ userId })
-      .sort({ createdAt: -1 })
-      .limit(10);
-    res.send(operations);
   });
 
   app.post('/api/operations/:user_id', async (req, res) => {
-    const { category, amount, receiptImageUrl, messageText, userId } = req.body;
-
-    const operation = new Operation({
-      category,
-      amount,
-      receiptImageUrl,
-      notes: messageText,
-      userId,
-    });
-
     try {
-      await operation.save();
+      const { category, amount, receiptImageUrl, messageText, userId } = req.body;
+
+      if (!userId || amount || category) {
+        throw new ErrorHandler('500', 'Wrong data format');
+      }
+
+      const operation = new Operation({
+        category,
+        amount,
+        receiptImageUrl,
+        notes: messageText,
+        userId,
+      });
+
+      const result = await operation.save();
       res.send(200);
     } catch (err) {
-      res.send(500, err);
+      console.log(err);
+      throw new ErrorHandler(500, 'Internal server error');
     }
   });
 };
