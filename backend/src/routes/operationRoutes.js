3c472c4948134784eda81bfd6caeba21078e4c0f
Mikhail Karpov <mihanikk@gmail.com>
2020-02-18 11:52:50 +0300

    added operations endpoints: month_top_10, by_category

--- backend/src/routes/operationRoutes.js
+++ backend/src/routes/operationRoutes.js
@@ -5,6 +5,56 @@ const { ErrorHandler } = require('../helpers/error');
 const Operation = mongoose.model('Operation');
 
 module.exports = app => {
+  app.get('/api/operations/:user_id/month_top_10', async (req, res) => {
+    try {
+      const userId = req.params.user_id;
+
+      if (!userId) {
+        throw new ErrorHandler(401, 'Missing user_id');
+      }
+
+      const date = new Date();
+      const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+      const operations = await Operation.find({ userId, createdAt: { $gte: firstDay } })
+        .sort({ amount: -1 })
+        .limit(10);
+
+      if (!operations) {
+        throw new ErrorHandler(500, 'Internal server error');
+      }
+
+      res.send(operations);
+    } catch (err) {
+      console.log(err);
+      throw new ErrorHandler(500, 'Internal server error');
+    }
+  });
+
+  app.get('/api/operations/:user_id/by_category', async (req, res) => {
+    try {
+      const userId = req.params.user_id;
+
+      if (!userId) {
+        throw new ErrorHandler(401, 'Missing user_id');
+      }
+
+      const operations = await Operation.aggregate([
+        { $match: { userId } },
+        { $group: { _id: '$category', amount: { $sum: '$amount' } } },
+        { $sort: { category: -1 } },
+      ]).sort({ amount: -1 });
+
+      if (!operations) {
+        throw new ErrorHandler(500, 'Internal server error');
+      }
+
+      res.send(operations);
+    } catch (err) {
+      console.log(err);
+      throw new ErrorHandler(500, 'Internal server error');
+    }
+  });
+
   app.get('/api/operations/:user_id/last_10', async (req, res) => {
     try {
       const userId = req.params.user_id;
